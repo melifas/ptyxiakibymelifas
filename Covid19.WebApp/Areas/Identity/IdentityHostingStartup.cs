4f41695bded3a834a37cef20bed54faa040429bf
Meletis Fassoulis <meletisfasoulis@yahoo.gr>
2021-03-20 18:35:04 +0200

    Modifications on covid Razor page

---
+++ Covid19.WebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,23 @@
+﻿using Covid19.WebApp.Areas.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ConfArch.Web.Areas.Identity.IdentityHostingStartup))]
+namespace ConfArch.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddTransient<IEmailSender, EmailSender>();
+
+            });
+        }
+    }
+}
--- Covid19.WebApp/Startup.cs
+++ Covid19.WebApp/Startup.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Covid19.WebApp.Areas.Identity;
 
 namespace Covid19.WebApp
 {
