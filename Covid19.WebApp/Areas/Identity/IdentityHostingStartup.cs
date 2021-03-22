584c4f21fc25d3c806dcc4b2dc4ad4d19a7bb297
Meletis Fassoulis <meletisfasoulis@yahoo.gr>
2021-03-22 20:05:39 +0200

    email

--- Covid19.WebApp/Areas/Identity/IdentityHostingStartup.cs
+++ Covid19.WebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -3,6 +3,7 @@ using Covid19.WebApp.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +16,8 @@ namespace Covid19.WebApp.Areas.Identity
         public void Configure(IWebHostBuilder builder)
         {
             builder.ConfigureServices((context, services) => {
+                services.AddTransient<IEmailSender, EmailSender>();
+
             });
         }
     }
